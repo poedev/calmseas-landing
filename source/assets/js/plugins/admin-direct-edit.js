b5e865cd4684905a78ee2b472717c89a49c1cfac
Poe Nguyen <tantan.bamboo123@gmail.com>
2015-07-24 17:40:03 +0700

    admin overlay

--- source/assets/js/plugins/admin-direct-edit.js
+++ source/assets/js/plugins/admin-direct-edit.js
@@ -1,22 +1,2 @@
 (function($, window){
-  $('body').append('<div class="editable-overlay"></div>');
-
-  $('.editable')
-    .on('mouseenter', function(){
-      var area = $(this);
-      console.log("IN");
-      $('.editable-overlay').stop().show().css({
-        'width': area.outerWidth(),
-        'height': area.outerHeight(),
-        'top': area.offset().top,
-        'left': area.offset().left
-      });
-
-    });
-  $('.editable-overlay')
-    .on('mouseleave', function(){
-      console.log("OUT");
-      $('.editable-overlay').stop().hide();
-    });
-
 }($, window));
